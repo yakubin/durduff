6f6af0b13c2aa71fda3f773a09e9a9c1eb43ff6a
Jakub Alba <jakub@yakubin.com>
2019-09-13 12:15:43 +0200

    prepend the prerelease version with a dash when invoked with "--version"

--- src/main.rs
+++ src/main.rs
@@ -39,13 +39,17 @@ use crate::verdictor::Verdictor;
 
 /// Get `dirdiff` version (as specified in Cargo.toml).
 fn get_version() -> String {
-    format!(
-        "{}.{}.{}{}",
+    let core = format!(
+        "{}.{}.{}",
         env!("CARGO_PKG_VERSION_MAJOR"),
         env!("CARGO_PKG_VERSION_MINOR"),
         env!("CARGO_PKG_VERSION_PATCH"),
-        option_env!("CARGO_PKG_VERSION_PRE").unwrap_or("")
-    )
+    );
+
+    match option_env!("CARGO_PKG_VERSION_PRE") {
+        Some(pre) => format!("{}-{}", core, pre),
+        None => core,
+    }
 }
 
 /// Result of dirdiff execution.
