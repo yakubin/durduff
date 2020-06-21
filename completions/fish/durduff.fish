d96fe1a7c7dc550783501dd880faaf99f31756ea
Jakub Alba <jakub@yakubin.com>
2020-06-21 12:25:00 +0200

    Update completion scripts

--- completions/fish/durduff.fish
+++ completions/fish/durduff.fish
@@ -1,6 +1,6 @@
 function print_args_wo_opts
     commandline -poc \
-        | awk 'NR==1 {next} /^(-b|--block-size|--color)$/{printf "%s ", $0; next} {print}' \
+        | awk 'NR==1 {next} /^(-b|--block-size|--color|--progress)$/{printf "%s ", $0; next} {print}' \
         | awk '/^-/{next} {print}'
 end
 
@@ -13,9 +13,9 @@ function num_of_args_wo_opts_is_at_most_1
 end
 
 complete -c durduff -s q -l brief --description "Report only when directories differ."
-complete -c durduff -s p -l progress --description "Show progress bar."
+complete -c durduff -l progress -xa "never always auto" --description "Print progress reports."
 complete -c durduff -l color -xa "never always auto" --description "Print output in color."
-complete -c durduff -l percent --description "Utf-8 percent-encode paths."
+complete -c durduff -s 0 -l null -xa "" --description "Print raw NUL-separated paths."
 complete -c durduff -s b -l block-size -xa "" --description "Read files in blocks of <block-size> bytes."
 complete -c durduff -s h -l help --description "Print help information and exit."
 complete -c durduff -l version --description "Print version information and exit."
